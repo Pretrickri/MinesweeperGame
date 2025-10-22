54397c03a032c4d28e77f031dc80b814737f0622
Patrick <66845017+Pretrickri@users.noreply.github.com>
2025-10-22 15:20:03 -0300

    Documented JavaScript scripts

--- backend/Tiles.js
+++ backend/Tiles.js
@@ -1,24 +1,56 @@
+/**
+ * Represents a Tile in a Minesweeper game.
+ * state: is the state in which that Tile is displayed (hidden, revealed, flagged)
+ * x: x coordinate of the Tile in a board
+ * y: y coordinate of the Tile in a board
+ * mine: boolean representing whether the Tile is a mine or not
+ * adjMines: number of mines in adjacent Tiles - this number should be set by the caller.
+ */
 export class Tile{
+    /**
+     * Constructs a Tile object.
+     * @param {number} x - x coordinate of the Tile in a board 
+     * @param {number} y - y coordinate of the Tile in a board
+     */
     constructor(x,y){
-        this.element = "hidden";
+        this.state = "hidden";
         this.x = x;
         this.y = y;
         this.mine = false;
         this.adjMines = 0;
     }
 
+    /**
+     * Gets the state of the Tile.
+     * @returns {string} - the state in which the Tile is currently.
+     */
     getElement(){
-        return this.element;
+        return this.state;
     }
-    setElement(element){
-        this.element = element;
+    /**
+     * Sets the state of the Tile.
+     * @param {string} state - the new state to se the Tile to. 
+     */
+    setElement(state){
+        this.state = state;
     }
+    /**
+     * Checks wheather the Tile is a mine or not.
+     * @returns {boolean}
+     */
     isMine(){
         return this.mine;
     }
+    /**
+     * Sets the Tile as a mine.
+     */
     setMine(){
         this.mine = true;
     }
+    /**
+     * Returns the number of adjacent Mines.
+     * @returns {number} - adjacent Mines
+     */
     nearbyMines(){
         return this.adjMines;
     }
--- backend/minesweeper.js
+++ backend/minesweeper.js
@@ -1,6 +1,15 @@
 import { Tile } from "./Tiles.js";
 
+/**
+ * A board is a 2D array of Tiles.
+ * @typedef {Tile[][]} Board 
+ */
 
+/**
+ * Build a square board of given size
+ * @param {number} size - number of Tiles in each row and column 
+ * @returns {Board} - size x size board
+ */
 export function BuildBoard(size){
     const board = [];
     for(let i=0; i<size; i++){
@@ -14,6 +23,14 @@ export function BuildBoard(size){
     return board;
 }
 
+/**
+ * Print an given board to the console. Shows coordinates and weather a Tile is a Mine or not.
+ * @param {Board} board - the board to print
+ * @example
+ * || (0,0) = false || (0,1) = true || (0,2) = false || 
+ * || (1,0) = false || (1,1) = false || (1,2) = false || 
+ * || (2,0) = true || (2,1) = false || (2,2) = false ||
+ */
 export function PrintBoard(board){
     for(let i=0; i<board.length; i++){
         let print_board = "|| ";
@@ -27,6 +44,16 @@ export function PrintBoard(board){
     }
 }
 
+/* UPGRADE
+   Mines should be added as we create the board, so we don't have to use a lot of computation to check
+   for adjacent mines after adding them randomly.
+   Additionally, we should find a way to avoid adding multiple mines to the same Tile in a more efficient way.
+*/ 
+/**
+ * Radomly adds mines to the given board.
+ * @param {Board} board - the board to add mines to. 
+ * @param {number} numberOfMines - number of mines to add to the board
+ */
 export function addMines(board, numberOfMines){
     for(let i=0; i<numberOfMines;i++){
         const randX = Math.floor(Math.random() * board.length);
